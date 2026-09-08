bd79bac58545dd73ceb03020c8f773033d3c3841
carolsantosmktdigital-gif <carolsantos.mktdigital@gmail.com>
2026-09-08 15:44:21 -0300

    MELHORIAS 25

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='powerfit-studio-v24';
+const CACHE='powerfit-studio-v25';
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.add('./')).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(self.clients.claim())});
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(event.request,copy));return r}).catch(()=>caches.match('./')))});
